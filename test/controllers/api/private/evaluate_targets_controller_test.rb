e45eeb06475cf524879f447e566564d2a15d7329
nenadpet <nenad.petronijevic86@gmail.com>
2017-10-08 18:09:12 +0200

    Add tests for api authentication

--- test/controllers/api/private/evaluate_targets_controller_test.rb
+++ test/controllers/api/private/evaluate_targets_controller_test.rb
@@ -5,4 +5,13 @@ class Api::Private::EvaluateTargetsControllerTest < ActionController::TestCase
     post :create, {}
     assert_response :unauthorized
   end
+
+  test "should allow authenticated" do
+    api_key = ApiKey.create_with_token!
+
+    request.headers["HTTP_AUTHORIZATION"] = ActionController::HttpAuthentication::Token.encode_credentials(api_key.token)
+
+    post :create, {}
+    assert_response :unprocessable_entity
+  end
 end
--- test/controllers/api/private/target_groups_controller_test.rb
+++ test/controllers/api/private/target_groups_controller_test.rb
@@ -5,4 +5,13 @@ class Api::Private::TargetGroupsControllerTest < ActionController::TestCase
     get :show, { country_code: "gb" }
     assert_response :unauthorized
   end
+
+  test "should allow authenticated" do
+    api_key = ApiKey.create_with_token!
+
+    request.headers["HTTP_AUTHORIZATION"] = ActionController::HttpAuthentication::Token.encode_credentials(api_key.token)
+
+    get :show, { country_code: "gb" }
+    assert_response :success
+  end
 end
